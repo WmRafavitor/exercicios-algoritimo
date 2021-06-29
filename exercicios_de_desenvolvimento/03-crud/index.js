048a50fcbbc7e10f1eeb85663b09da13f39d2e54
Rafael Vitor <wmrafavitor@gmail.com>
2021-06-29 11:29:43 -0300

    finalizado a logica do crud

--- exercicios_de_desenvolvimento/03-crud/index.js
+++ exercicios_de_desenvolvimento/03-crud/index.js
@@ -13,6 +13,20 @@ function apagaReginstro(id) {
     }
 }
 
+function editaRegistro(id) {
+    $("#modalRegistro").modal("show");
+
+    dados.forEach(function(item) {
+        if(item.ID == id) {
+            $("hdID").val(item.ID);
+            $("#txtNome").val(item.nome);
+            $("#txtSobreNome").val(item.sobreNome);
+            $("#txtDtNascimento").val(item.dtNascimento.substr(6, 4) + "-" + item.dtNascimento.substr(3, 2) + "-" + item.dtNascimento.substr(0, 2));
+            $("#txtFormacao").val(item.formacao);
+        }
+    })
+}
+
 function populaTabela() {
     if(Array.isArray(dados)) {
         
@@ -27,7 +41,7 @@ function populaTabela() {
                 <td>${item.sobreNome}</td>
                 <td>${item.dtNascimento}</td>
                 <td>${item.formacao}</td>
-                <td><button type="button" class="btn btn-primary"><i class="fas fa-edit"></i></button></td>
+                <td><button type="button" class="btn btn-primary" onclick="javascript:editaRegistro(${item.ID});"><i class="far fa-edit"></i></button></td>
                 <td><button tupe="button" class="btn btn-danger" onclick="javascript:apagaReginstro(${item.ID});"><i class="far fa-trash-alt"></i></button></td>
             </tr>`)                                        
         })
@@ -43,25 +57,41 @@ $(function () {
     }
 
     $("#btnSalvar").click(function() {
+
+        let _id = $("#hdID").val();
         let nome = $("#txtNome").val();
         let sobreNome= $("#txtSobreNome").val();
         let dtNascimento = new Date($("#txtDtNascimento").val()).toLocaleDateString("pt-br", {timeZone: "UTC"}); //A VARIÁVEL RECEBE UM NEW DATE PARA CONVERTER A DATA EM HORÁRIO BRASILEIRO
         let formacao = $("#txtFormacao").val();
 
-        let registro = {};
+        if(!_id || _id == "0") {
+            let registro = {};
+            registro.nome = nome;
+            registro.sobreNome = sobreNome;
+            registro.dtNascimento = dtNascimento;
+            registro.formacao = formacao; 
 
-        registro.ID = (dados.length + 1);
-        registro.nome = nome;
-        registro.sobreNome = sobreNome;
-        registro.dtNascimento = dtNascimento;
-        registro.formacao = formacao;   
+            dados.push(registro);
+            registro.ID = (dados.length + 1);
+        } else {
+            dados.forEach(function(item) {
+                if(item.ID == _id) {
+                    item.nome = nome;
+                    item.sobreNome = sobreNome;
+                    item.dtNascimento = dtNascimento;
+                    item.formacao = formacao;
+                }
+            })
+        }
 
-        dados.push(registro);
+
+
 
         alert("Registro salvo com sucesso");
         $("#modalRegistro").modal("hide");
 
         //LIMPEZA DOS CAMPOS
+        $("hdID").val("0");
         $("#txtNome").val("");
         $("#txtSobreNome").val("");
         $("#txtDtNascimento").val("");
